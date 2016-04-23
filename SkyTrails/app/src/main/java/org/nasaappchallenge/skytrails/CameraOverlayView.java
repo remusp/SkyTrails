62c6db11526a8417127241f2f7f925637d03246c
Dragos Nutu <tuxpr0@yahoo.ro>
2016-04-23 19:02:08 +0300

    [dummy-ticket-id] :D - fixed location;

--- SkyTrails/app/src/main/java/org/nasaappchallenge/skytrails/CameraOverlayView.java
+++ SkyTrails/app/src/main/java/org/nasaappchallenge/skytrails/CameraOverlayView.java
@@ -21,216 +21,253 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class CameraOverlayView extends View implements SensorEventListener, LocationListener {
 
-	private static final String TAG = "CameraOverlayView";
-	private Location lastLocation = null;
-	float[] gravity;
-	float[] geomag;
+    private static final String TAG = "CameraOverlayView";
+    private static final long MIN_TIME_BW_UPDATES = 100;
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 100f;
+    private Location lastLocation = null;
+    float[] gravity;
+    float[] geomag;
+    private boolean canGetLocation;
+    private double latitude;
+    private double longitude;
+    private Location location;
 
-	public CameraOverlayView(Context context, AttributeSet attrs) {
-		super(context, attrs);
+    public CameraOverlayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-		SensorManager sensors = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-		Sensor accelSensor = sensors.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		Sensor compassSensor = sensors.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-		Sensor gyroSensor = sensors.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        SensorManager sensors = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        Sensor accelSensor = sensors.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Sensor compassSensor = sensors.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor gyroSensor = sensors.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-		boolean isAccelAvailable = sensors.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
-		boolean isCompassAvailable = sensors.registerListener(this, compassSensor, SensorManager.SENSOR_DELAY_NORMAL);
-		boolean isGyroAvailable = sensors.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        boolean isAccelAvailable = sensors.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        boolean isCompassAvailable = sensors.registerListener(this, compassSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        boolean isGyroAvailable = sensors.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-		LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-		Criteria criteria = new Criteria();
-		criteria.setAccuracy(Criteria.ACCURACY_FINE);
-		criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
 
-		String best = locationManager.getBestProvider(criteria, true);
+        String best = locationManager.getBestProvider(criteria, true);
 
-		if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) !=
-				PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
-				!= PackageManager.PERMISSION_GRANTED) {
-			// TODO: Consider calling
-			//    ActivityCompat#requestPermissions
-			// here to request the missing permissions, and then overriding
-			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-			//                                          int[] grantResults)
-			// to handle the case where the user grants the permission. See the documentation
-			// for ActivityCompat#requestPermissions for more details.
-			return;
-		}
-		locationManager.requestLocationUpdates(best, 100, 0, this);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(best, 100, 0, this);
 
-		lastLocation = getLastBestLocation(context, locationManager);
-	}
+        lastLocation = getLastBestLocation(context, locationManager);
+    }
 
-	protected void onDraw(Canvas canvas) {
-		if (lastLocation == null || gravity == null || geomag == null) {
-			return;
-		}
+    protected void onDraw(Canvas canvas) {
+        if (lastLocation == null || gravity == null || geomag == null) {
+            return;
+        }
 
-		drawMyPoint(lastLocation, mountWashington, canvas);
-		drawMyPoint(lastLocation, mountWashington2, canvas);
-	}
+        drawMyPoint(lastLocation, mountWashington, canvas);
+        drawMyPoint(lastLocation, mountWashington2, canvas);
+    }
 
-	private void drawMyPoint(Location location1, Location location2, Canvas canvas){
-		float curBearingToMW = location1.bearingTo(location2);
+    private void drawMyPoint(Location location1, Location location2, Canvas canvas) {
+        float curBearingToMW = location1.bearingTo(location2);
 
-		// compute rotation matrix
-		float rotation[] = new float[9];
-		float identity[] = new float[9];
-		boolean gotRotation = SensorManager.getRotationMatrix(rotation,
-				identity, gravity, geomag);
+        // compute rotation matrix
+        float rotation[] = new float[9];
+        float identity[] = new float[9];
+        boolean gotRotation = SensorManager.getRotationMatrix(rotation,
+                identity, gravity, geomag);
 
-		if (gotRotation) {
-			float cameraRotation[] = new float[9];
-			// remap such that the camera is pointing straight down the Y axis
-			SensorManager.remapCoordinateSystem(rotation, SensorManager.AXIS_X,
-					SensorManager.AXIS_Z, cameraRotation);
+        if (gotRotation) {
+            float cameraRotation[] = new float[9];
+            // remap such that the camera is pointing straight down the Y axis
+            SensorManager.remapCoordinateSystem(rotation, SensorManager.AXIS_X,
+                    SensorManager.AXIS_Z, cameraRotation);
 
-			// orientation vector
-			float orientation[] = new float[3];
-			SensorManager.getOrientation(cameraRotation, orientation);
-			if (gotRotation) {
-				cameraRotation = new float[9];
-				// remap such that the camera is pointing along the positive direction of the Y axis
-				SensorManager.remapCoordinateSystem(rotation, SensorManager.AXIS_X,
-						SensorManager.AXIS_Z, cameraRotation);
+            // orientation vector
+            float orientation[] = new float[3];
+            SensorManager.getOrientation(cameraRotation, orientation);
+            if (gotRotation) {
+                cameraRotation = new float[9];
+                // remap such that the camera is pointing along the positive direction of the Y axis
+                SensorManager.remapCoordinateSystem(rotation, SensorManager.AXIS_X,
+                        SensorManager.AXIS_Z, cameraRotation);
 
-				// orientation vector
-				orientation = new float[3];
-				SensorManager.getOrientation(cameraRotation, orientation);
+                // orientation vector
+                orientation = new float[3];
+                SensorManager.getOrientation(cameraRotation, orientation);
 
-				float verticalFOV = 47.4366F;
-				float horizontalFOV = 60.0848F;
+                float verticalFOV = 47.4366F;
+                float horizontalFOV = 60.0848F;
 
-				// use roll for screen rotation
-				canvas.rotate((float) (0.0f - Math.toDegrees(orientation[2])));
-				// Translate, but normalize for the FOV of the camera -- basically, pixels per degree, times degrees == pixels
-				float dx = (float) ((canvas.getWidth() / horizontalFOV) * (Math.toDegrees(orientation[0]) - curBearingToMW));
-				float dy = (float) ((canvas.getHeight() / verticalFOV) * Math.toDegrees(orientation[1]));
+                // use roll for screen rotation
+                canvas.rotate((float) (0.0f - Math.toDegrees(orientation[2])));
+                // Translate, but normalize for the FOV of the camera -- basically, pixels per degree, times degrees == pixels
+                float dx = (float) ((canvas.getWidth() / horizontalFOV) * (Math.toDegrees(orientation[0]) - curBearingToMW));
+                float dy = (float) ((canvas.getHeight() / verticalFOV) * Math.toDegrees(orientation[1]));
 
-				// wait to translate the dx so the horizon doesn't get pushed off
-				canvas.translate(0.0f, 0.0f - dy);
+                // wait to translate the dx so the horizon doesn't get pushed off
+                canvas.translate(0.0f, 0.0f - dy);
 
-				Paint paint = new Paint();
-				paint.setColor(Color.RED);
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
 
-				// make our line big enough to draw regardless of rotation and translation
-				canvas.drawLine(0f - canvas.getHeight(), canvas.getHeight() / 2, canvas.getWidth() + canvas.getHeight(), canvas.getHeight() / 2, paint);
+                // make our line big enough to draw regardless of rotation and translation
+                canvas.drawLine(0f - canvas.getHeight(), canvas.getHeight() / 2, canvas.getWidth() + canvas.getHeight(), canvas.getHeight() / 2, paint);
 
 
-				// now translate the dx
-				canvas.translate(0.0f - dx, 0.0f);
+                // now translate the dx
+                canvas.translate(0.0f - dx, 0.0f);
 
-				// draw our point -- we've rotated and translated this to the right spot already
-				canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 8.0f, paint);
+                // draw our point -- we've rotated and translated this to the right spot already
+                canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, 8.0f, paint);
 
-				// Undo
-				canvas.translate(0.0f + dx, 0.0f);
-				canvas.translate(0.0f, 0.0f + dy);
-				canvas.rotate((float) (0.0f + Math.toDegrees(orientation[2])));
-			}
-		}
-	}
+                // Undo
+                canvas.translate(0.0f + dx, 0.0f);
+                canvas.translate(0.0f, 0.0f + dy);
+                canvas.rotate((float) (0.0f + Math.toDegrees(orientation[2])));
+            }
+        }
+    }
 
-	@Override
-	public void onLocationChanged(Location location) {
-		lastLocation = location;
-	}
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLocation = location;
+    }
 
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-	}
+    }
 
-	@Override
-	public void onProviderEnabled(String provider) {
+    @Override
+    public void onProviderEnabled(String provider) {
 
-	}
+    }
 
-	@Override
-	public void onProviderDisabled(String provider) {
+    @Override
+    public void onProviderDisabled(String provider) {
 
-	}
+    }
 
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		// If the sensor data is unreliable return
-		if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
-			return;
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // If the sensor data is unreliable return
+//		if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
+//			return;
 
-		// Gets the value of the sensor that has been changed
-		switch (event.sensor.getType()) {
-			case Sensor.TYPE_ACCELEROMETER:
-				gravity = event.values.clone();
-				break;
-			case Sensor.TYPE_MAGNETIC_FIELD:
-				geomag = event.values.clone();
-				break;
-		}
+        // Gets the value of the sensor that has been changed
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                gravity = event.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                geomag = event.values.clone();
+                break;
+        }
 
-		this.invalidate();
-	}
+        this.invalidate();
+    }
 
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-	}
+    }
 
-	/**
-	 * Get last location
-	 *
-	 * @return last location
-	 */
-	private Location getLastBestLocation(Context context, LocationManager locationManager) {
+    /**
+     * Get last location
+     *
+     * @return last location
+     */
+    private Location getLastBestLocation(Context context, LocationManager locationManager) {
 
-		if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-			// TODO: Consider calling
-			//    ActivityCompat#requestPermissions
-			// here to request the missing permissions, and then overriding
-			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-			//                                          int[] grantResults)
-			// to handle the case where the user grants the permission. See the documentation
-			// for ActivityCompat#requestPermissions for more details.
-			return null;
-		}
-		Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		Location locationNet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        try {
+//            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
-		long GPSLocationTime = 0;
-		if (locationGPS != null) {
-			GPSLocationTime = locationGPS.getTime();
-		}
+            // getting GPS status
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-		long NetLocationTime = 0;
+            // getting network status
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-		if (locationNet != null) {
-			NetLocationTime = locationNet.getTime();
-		}
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled) {
+                    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+//                        return TODO;
 
-		if (GPSLocationTime - NetLocationTime > 0) {
-			return locationGPS;
-		} else {
-			return locationNet;
-		}
-	}
+                    }
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network Enabled");
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS", "GPS Enabled");
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+            }
 
-	private final static Location mountWashington = new Location("manual");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	static {
-		mountWashington.setLatitude(46.775472d);
-		mountWashington.setLongitude(23.595470d);
-		mountWashington.setAltitude(1916.5d);
-	}
+        return location;
+    }
 
-	private final static Location mountWashington2 = new Location("manual2");
+    private final static Location mountWashington = new Location("manual");
 
-	static {
-		mountWashington.setLatitude(46.788856d);
-		mountWashington.setLongitude(23.708677d);
-		mountWashington.setAltitude(1916.5d);
-	}
+    static {
+        mountWashington.setLatitude(46.775472d);
+        mountWashington.setLongitude(23.595470d);
+        mountWashington.setAltitude(1916.5d);
+    }
+
+    private final static Location mountWashington2 = new Location("manual2");
+
+    static {
+        mountWashington.setLatitude(46.788856d);
+        mountWashington.setLongitude(23.708677d);
+        mountWashington.setAltitude(1916.5d);
+    }
 }
